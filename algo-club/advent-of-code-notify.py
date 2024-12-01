b1397ceb4985372728b605d7512b5f63d0a55685
Osip Fatkullin <osip.fatkullin@gmail.com>
2024-12-01 01:32:05 +0100

    aoc: Send message to Telegram instead of Discord

--- algo-club/advent-of-code-notify.py
+++ algo-club/advent-of-code-notify.py
@@ -3,28 +3,28 @@ import os
 import random
 import re
 import time
-from datetime import datetime
+from datetime import datetime, timezone
 
 import requests
 
 AOC_URL = 'https://adventofcode.com'
+TELEGRAM_BOT_TOKEN = os.environ['TELEGRAM_BOT_TOKEN']
+TELEGRAM_CHAT_ID = os.environ['TELEGRAM_CHAT_ID']
+TELEGRAM_API_URL = f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}'
 
-# 0. Select random greeting and wait for Midnight EST
-greeting = random.choice([
-    'Хо-хо-хо!',
-    'Хэй!',
-    'Йоу!',
-    'Привет!',
-    'А я к вам с подарочком!',
-    ':yawning_face: Привет всем кто уже проснулся!',
-    'На часах ровно... время выкладывать задачку!',
-    ':wave::robot:',
-    'Приветствую, человеки.',
-    'Псс... Как насчёт заработать немного :star:?',
-    '[ERROR: GREETING_NOT_FOUND]',
-])
+EMOJI_ELF = '\U0001F9DD'
+EMOJI_SMILE = '\U0001F642'
+EMOJI_CHRISTMAS_TREE = '\U0001F384'
+EMOJI_STAR = '\U00002B50'
+EMOJI_YAWNING = '\U0001F971'
+EMOJI_ONE = '\U0000261D\U0000FE0F'
+EMOJI_TWO = '\U0000270C\U0000FE0F'
+EMOJI_CROSSED = '\U0001FAF0'
 
-start = datetime.utcnow()
+# 0. Set greeting and wait for Midnight EST
+greeting = "П-привет, программисты! *нервно теребит колпак* Сегодня первый день Advent of Code, и я так волнуюсь!!! Помогите нам спасти Рождество!!!"
+
+start = datetime.now(timezone.utc)
 target_time = datetime(start.year, start.month, start.day, 5, 0, 1)  # Midnight EST (UTC-5)
 remaining = target_time - start
 remaining_seconds = remaining.total_seconds()
@@ -32,7 +32,7 @@ remaining_seconds = remaining.total_seconds()
 if remaining_seconds > 0:
     print(f'Sleep for {remaining}')
     time.sleep(remaining_seconds)
-print(f'Started at {datetime.utcnow().time()}')
+print(f'Started at {datetime.now(timezone.utc).time()}')
 
 # 1. Retrieve the daily task and parse it
 year, day = str(start.year), str(start.day)
@@ -44,29 +44,38 @@ title = re.search('<article class="day-desc"><h2>--- (.+) ---</h2>', response.te
 print(title)
 
 # 2. Build message
-data = {
-    "thread_name": title.replace(': ', ' - '),
-    "content": f'{greeting} Сегодня {day}-й день Advent of Code!\n'
-               'В этом треде можно задавать вопросы по задаче, делиться идеями и решениями.\n'
-               'Если задача понравилась, жми :christmas_tree:! '
-               'Разберём задачи которые наберут больше всего "ёлок".\n'
-               '\n'
-               'Если хочешь поделиться решением, будет классно если дополнительно укажешь:\n'
-               ':tools: Язык на котором оно написано\n'
-               ':timer: Сложность по времени _(если можешь посчитать)_\n'
-               ':floppy_disk: Сложность по памяти _(если можешь посчитать)_\n'
-               '\n'
-               '> :robot: Старайся не постить спойлеры к решению в день когда задача опубликована. '
-               'Если очень хочется, окружи спойлер двумя вертикальными палками `||`, тогда текст будет скрыт (ссылки можно не скрывать за спойлеры). '
-               'На следующий день — пожалуйста, тред открыт для спойлеров :slight_smile:',
-    "embeds": [
-        {
-            "title": title,
-            "url": day_url,
-            "color": 16711680,  # https://convertingcolors.com/decimal-color-16711680.html
-        }
-    ],
-}
+def escape(text: str) -> str:
+    special_chars = ['_', '*', '[', ']', '(', ')', '~', '`', '>', '#', '+', '-', '=', '|', '{', '}', '.', '!']
+    for char in special_chars:
+        if char in text:
+            text = text.replace(char, f'\\{char}')
+    return text
 
-# 3. Send the message to Discord
-response = requests.post(os.environ['DISCORD_WEBHOOK'], json=data)
+message = (
+    f'{EMOJI_CHRISTMAS_TREE} *[{escape(title)}]({day_url})*\n'
+    '\n'
+    f'{EMOJI_ELF} \\(вбегает молодой эльф\\): {escape(greeting)}\n'
+    '\n'
+    f'**>*На всякий случай напомню правила*\n'
+    '> \n'
+    f'>{EMOJI_ONE} Обсуждаем задачи в комментариях к посту\n'
+    f'>{EMOJI_TWO} Прячь все спойлеры ||вот так|| в день публикации задачи\\. '
+    f'На следующий день тред открыт для спойлеров {EMOJI_SMILE}\n'
+    f'>{EMOJI_CROSSED} Помни про @aoc\\_club\\_chat, где можно общаться без привязки к задачам\\.||'
+)
+
+print(message)
+
+# 3. Send the message to Telegram
+response = requests.post(
+    f'{TELEGRAM_API_URL}/sendMessage',
+    json={
+        'chat_id': TELEGRAM_CHAT_ID,
+        'text': message,
+        'parse_mode': 'MarkdownV2',
+        'link_preview_options': {
+            'is_disabled': False,
+        },
+    }
+)
+response.raise_for_status()
